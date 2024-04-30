813e7162f1ef8096903a0c22f25bc8e3a141a54b
4rron <air272@live.com>
2024-04-30 19:09:09 -0400

    Remove light mode toggle

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,9 +1,5 @@
 import LightModeToggle from "../light-mode-toggle/light-mode-toggle";
 
 export default function Header() {
-    return (
-        <div className="header">
-            <LightModeToggle />
-        </div>
-    );
+    return <div className="header">{/* <LightModeToggle /> */}</div>;
 }
